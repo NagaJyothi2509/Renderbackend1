8e4360566e5eaa9c6d37cd7d989784b7df7061dd
Jyothi K <jyothik@Jyothis-MacBook-Air.local>
2023-08-01 12:19:06 +0530

    added logs for backend file

--- routes/contact.js
+++ routes/contact.js
@@ -483,6 +483,31 @@ const router = require("express").Router();
 // Create Contact
 router.post("/contact", auth, async (req, res) => {
   // ... existing code for creating a new contact ...
+  console.log(req.body);
+  const {error}=validateContact(req.body);
+  console.log(error);
+
+    if(error){
+        return res.status(400).json({error:error.details[0].message})
+
+    }
+    const{name,address,email,phone}=req.body;
+    try{
+         const newContact=new Contact({
+            name,
+            address,
+            email,
+            phone,
+            postedBy:req.user._id,
+         });
+    const result=await newContact.save();
+    return res.status(201).json({...result._doc});
+
+    }catch(err){
+        console.log(err, 'in catch block');
+        return res.status(400).json({message: 'Not Found'});
+    }
+
 });
 
 // Fetch paginated contacts or search contacts
@@ -547,16 +572,82 @@ router.get("/mycontacts", auth, async (req, res) => {
 // Update contact
 router.put("/contact/:id", auth, async (req, res) => {
   // ... existing code for updating a contact ...
+  const { id } = req.body;
+  
+    if (!id) return res.status(400).json({ error: "no id specified." });
+    if (!mongoose.isValidObjectId(id))
+      return res.status(400).json({ error: "please enter a valid id" });
+  
+    try {
+      const contact = await Contact.findOne({ _id: id });
+  
+      if (req.user._id.toString() !== contact.postedBy._id.toString())
+        return res
+          .status(401)
+          .json({ error: "you can't edit other people contacts!" });
+  
+      const updatedData = { ...req.body, id: undefined };
+      const result = await Contact.findByIdAndUpdate(id, updatedData, {
+        new: true,
+      });
+  
+      return res.status(200).json({ ...result._doc });
+    } catch (err) {
+      console.log(err);
+    }
+
 });
 
 // Delete contact
 router.delete("/contact/:id", auth, async (req, res) => {
   // ... existing code for deleting a contact ...
+  const { id } = req.params;
+  
+    if (!id) return res.status(400).json({ error: "no id specified." });
+  
+    if (!mongoose.isValidObjectId(id))
+      return res.status(400).json({ error: "please enter a valid id" });
+    try {
+      const contact = await Contact.findOne({ _id: id });
+      if (!contact) return res.status(400).json({ error: "no contact found" });
+  
+      if (req.user._id.toString() !== contact.postedBy._id.toString())
+        return res
+          .status(401)
+          .json({ error: "you can't delete other people contacts!" });
+  
+      const result = await Contact.deleteOne({ _id: id });
+      const myContacts = await Contact.find({ postedBy: req.user._id }).populate(
+        "postedBy",
+        "-password"
+      );
+  
+      return res
+        .status(200)
+        .json({ ...contact._doc, myContacts: myContacts.reverse() });
+    } catch (err) {
+      console.log(err);
+    }
 });
 
 // Get a single contact
 router.get("/contact/:id", auth, async (req, res) => {
   // ... existing code for fetching a single contact ...
+
+  const { id } = req.params;
+  
+  if (!id) return res.status(400).json({ error: "no id specified." });
+
+  if (!mongoose.isValidObjectId(id))
+    return res.status(400).json({ error: "please enter a valid id" });
+
+  try {
+    const contact = await Contact.findOne({ _id: id });
+
+    return res.status(200).json({ ...contact._doc });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
